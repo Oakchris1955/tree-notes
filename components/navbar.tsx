accc30dd57d3b4a53ed23982f047da96c53905bb
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-04-17 14:36:10 +0300

    Navigation bar changes depending on if user is logged in or not

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,47 @@
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+
+enum authTokenEnum {
+	True,
+	False,
+	Unknown
+}
+
+function renderAuthComponets(hasAuthToken: authTokenEnum): JSX.Element {
+	switch (hasAuthToken) {
+		case authTokenEnum.True:
+			return (
+				<a href="#" onClick={() => {Cookies.remove("authToken");location.reload()}}>Logout</a>
+			);
+		case authTokenEnum.False:
+			return (
+				<>
+					<a href="/login">Login</a>
+					<a href="/signup">Signup</a>
+				</>
+			);
+		default:
+			return (
+				<></>
+			)
+	}
+}
 
 export default function NavigationBar() {
+	const [hasAuthToken, setHasAuthToken] = useState(authTokenEnum.Unknown);
+
+	useEffect(() => {
+		setHasAuthToken(typeof Cookies.get("authToken") === "string" ? authTokenEnum.True : authTokenEnum.False)
+	}, [Cookies])
+
 	return (
 		<div className="navBar">
 			<div>
 				<a href="/">Main Page</a>
 			</div>
 			<div style={{marginLeft: "auto"}}>
-				<a href="#" onClick={() => {Cookies.remove("authToken");location.reload()}}>Logout</a>
+				{renderAuthComponets(hasAuthToken)}
+				
 			</div>
 			<hr/>
 		</div>
